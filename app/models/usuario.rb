e1e14003b12a0a4b4392e5a1a0db7422942769ee
Erik Vasquez <erikesneider@gmail.com>
2021-10-07 13:54:18 -0500

    1-

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,8 @@
 class Usuario < ApplicationRecord
 	validates_presence_of :nombre
-	
+	validates_presence_of :apellido
+	validates_presence_of :telefono
+
 	def self.search(query)
 		where(["nombre LIKE ?", "%#{query}%"])
 	end
